d5b0800ae68ff8ffab1cc0b702d2a142ef8ca5a3
Fahimnur Alam <fahim@codexi.com>
2016-09-13 03:24:32 -0400

    🎩 Some JavaScript Magic - Extend the waypoint function - Add active menu class item - Dynamic menu

--- themes/whurley/static/js/site.js
+++ themes/whurley/static/js/site.js
@@ -33,12 +33,43 @@ $(function(){
     player.api('setVolume', 0);
     console.log('shh....');
   });
+  
+  var mainWaypoints = ['digital-citizen', 'entrepreneur-innovator', 'speaker-author'];
+  var wayPointContents = [];
+  var menu = $('#exCollapsingNavbar2').clone();
+  
+  for(var i=0; i<mainWaypoints.length;i++){
+    var waypointElement =  document.getElementById(mainWaypoints[i]);
+    wayPointContents.push(waypointElement.innerHTML);
 
-  var waypointElement = 'digital-citizen';
-  var waypoint = new Waypoint({
-    element: document.getElementById(waypointElement),
-    handler: function(direction) {
-      console.log(waypointElement)
-    }
-  })
+    new Waypoint({
+      element: waypointElement,
+      handler: function (dir) {
+        var currentWayPoint = parseInt($(this)[0].key.split("-")[1]);
+        for (var x = 0; x < mainWaypoints.length; x++) {
+          var resetElem = document.getElementById(mainWaypoints[x]);
+          resetElem.innerHTML = wayPointContents[x];
+        }
+        menu.addClass('active-pane-menu');
+        menu[0].style.marginTop = "-100px";
+        menu[0].style.paddingBottom = "100px";
+        var activeMenu = mainWaypoints[currentWayPoint];
+        $(menu.find('.navbar-nav')[0].children).each(function(i, nav){
+          var item = (($(nav).find('a:first').attr('href')).substring(2));
+          if (item == activeMenu) {
+            $(nav).addClass('active-nav-item');
+          }
+          else {
+            $(nav).removeClass('active-nav-item');
+          }
+        });
+        $(this)[0].element.innerHTML = menu[0].outerHTML + $(this)[0].element.innerHTML;
+      },
+      offset: "20px"
+      
+    });
+  }
+  
+
+  
 });
