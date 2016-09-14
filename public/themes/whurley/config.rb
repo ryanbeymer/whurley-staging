18f159fa63306c6a138eff2ed6d4f1c2d69e82ce
Ryan Beymer <ryanbeymer@gmail.com>
2016-09-14 14:28:23 -0600

    remove public to make room for subtree

--- public/themes/whurley/config.rb
+++
@@ -1,9 +0,0 @@
-
-css_dir = "static/css" # by Compass.app 
-sass_dir = "static/css/scss" # by Compass.app 
-images_dir = "images" # by Compass.app 
-output_style = :expanded # by Compass.app 
-relative_assets = false # by Compass.app 
-line_comments = false # by Compass.app 
-sass_options = {:debug_info=>false} # by Compass.app 
-sourcemap = true # by Compass.app 
